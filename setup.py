3fb189f0b6529984a70dda5a8e7e4730ef594969
Alexander Ryazanov <alryaz@xavux.com>
2021-06-28 08:03:49 +0300

    chore: version bump (v0.0.14 -> v0.0.15)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 project_id = "inter-rao-energosbyt-python"
 setuptools.setup(
     name=project_id,
-    version="0.0.14",
+    version="0.0.15",
     author="Alexander Ryazanov",
     author_email="alryaz@xavux.com",
     description="Inter RAO API bindings for python",
